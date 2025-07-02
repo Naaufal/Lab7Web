6e050f7370a48d28f949189a53e27642d9a51fc3
naaufal <muhammadnaufalaliakbar985@gmail.com>
2025-07-02 12:33:56 +0700

    Lab7Web

---
+++ app/Controllers/Page.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Page extends BaseController
+{
+    public function artikel()
+    {
+        $data = [
+            'title' => 'Halaman Artikel',
+            'content' => 'ini adalah halaman artikel'
+        ];
+
+        return view('artikel', $data);
+    }
+
+    public function about()
+    {
+        return view('about', [
+            'title' => 'Halaman About',
+            'content' => 'Ini adalah halaman about yang menjelaskan tentang isi halaman ini.'
+        ]);
+    }
+
+    public function contact()
+    {
+        $data = [
+            'title' => 'Halaman Contact',
+            'content' => 'Ini adalah halaman contact'
+        ];
+        
+        return view('contact', $data); 
+    }
+
+    public function faqs()
+    {
+        echo "ini halaman faqs";
+    }
+}
